241f2774f67250ff67bb9f3f5011017751c8d973
JeanFeitani <jeanls.folks@gmail.com>
2024-02-25 13:25:34 -0300

    3rd-commit-node-p

--- Projeto Node/src/controllers/UsersController.js
+++ Projeto Node/src/controllers/UsersController.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
     create(request, response){
         const {name, email, isAdmin} = request.body
+
+        if(!name){
+            throw new AppError("nome é obrigatório")
+        }
+
         response.status(201).json({name})
     }
 }
