40656012c5804382616ad5bc93c8fecf5dd3a8ad
JeanFeitani <jeanls.folks@gmail.com>
2024-02-25 19:03:31 -0300

    4th-commit-node-p

--- Projeto Node/src/server.js
+++ Projeto Node/src/server.js
@@ -2,12 +2,14 @@ require("express-async-errors")
 const AppError = require("./utils/AppError")
 const express = require("express");
 const routes = require("./routes")
+const migrationsRun = require("./database/migrations")
 
 const app = express();
+
+migrationsRun()
+
 app.use(express.json())
-
 app.use(routes)
-
 app.use((error, request, response, next) => {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
