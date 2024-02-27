9f90a5585260dbc02db5dec18c25f33435126a51
JeanFeitani <jeanls.folks@gmail.com>
2024-02-27 19:23:55 -0300

    1st-commit-react-p

---
+++ React Project/React First Project/src/styles/global.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    background-color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+    color: ${({theme}) => theme.COLORS.WHITE};
+
+};
+
+body, button, textarea, input {
+    font-family: 'Roboto', serif;
+    font-size: 6rem;
+    outline: none;
+}
+
+a {
+    text-decoration: none;
+
+}
+
+button, a{
+    cursor: pointer;
+    transition: filter 0.2s
+} 
+
+button:hover, a:hover {
+    filter:brightness(0.9);
+} 
+
+
+
+`
+
