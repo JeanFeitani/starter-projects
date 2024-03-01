1430576a1209a8a6a78473fe6a5117f1a111ced7
JeanFeitani <jeanls.folks@gmail.com>
2024-03-01 17:06:55 -0300

    imageUpload-implementation-node-p

--- Projeto Node/src/server.js
+++ Projeto Node/src/server.js
@@ -3,13 +3,17 @@ const AppError = require("./utils/AppError")
 const express = require("express");
 const routes = require("./routes")
 const migrationsRun = require("./database/sqlite/migrations")
-
-const app = express();
+const cors = require("cors")
 
 migrationsRun()
 
+const app = express();
+app.use(cors())
 app.use(express.json())
+
 app.use(routes)
+
+
 app.use((error, request, response, next) => {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
