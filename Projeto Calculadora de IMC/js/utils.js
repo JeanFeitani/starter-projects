69724684dc436dc827ecd000071e50a31671f406
JeanFeitani <jeanls.folks@gmail.com>
2024-04-10 11:33:09 -0300

    starter-projects

--- Projeto Calculadora de IMC/js/utils.js
+++
@@ -1,9 +0,0 @@
-function notAnumber(value) {
-    return isNaN(value) || value == ""
-}
-
-function calculateIMC(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
-}
-
-export {calculateIMC, notAnumber}
